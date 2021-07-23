8b0a01b162c110aae5b6435624a377e544c9a63c
Michal Richter <jisaz9q@Zakkarys-MBP.attlocal.net>
2021-07-23 16:29:36 -0500

    created hero service and injected into hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { HeroDetailsComponent } from './components/hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
