44d9df3917b3f93bc0e9c7dc2ce17a027e9788d8
Michal Richter <jisaz9q@Zakkarys-MacBook-Pro.local>
2021-07-26 09:48:15 -0500

    made data calls observable

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../hero';
-import { HEROES } from '../../mock-heroes';
 import { HeroService } from '../../services/hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -24,7 +24,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
